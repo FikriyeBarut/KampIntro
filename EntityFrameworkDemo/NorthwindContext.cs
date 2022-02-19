28501219179f62e22bac46466c09b33d04fd32e7
fikri <fikriye.barut9@gmail.com>
2022-02-19 15:49:10 +0300

    19 şubat

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext:DbContext//context bizim veritabanındaki tablolarımızla classlarımızı ,l,ikilendirmeye yarıyor
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    internal class Program
+    {
+        //entity framework --bir ORM ->Object relational mapping(nesneleri veritabanıyla bağdaştırma)
+        static void Main(string[] args)
+        {
+            //GetAll();
+            GetProductsByCategory(1);//1 numaralı categoryıd istiyorum
+        }
+
+        private static void GetAll()
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+
+            foreach (var product in northwindContext.Products)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext northwindContext = new NorthwindContext();
+            var result = northwindContext.Products.Where(p => p.CategoryId == categoryId);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
